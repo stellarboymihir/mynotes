587258bdbec8180a1f815b52ba541fa74c7bb73c
stellarboymihir <stellarboymihir00@gmail.com>
2023-10-16 12:51:11 +0530

    Preparing to Create New Notes

--- lib/test/auth_test.dart
+++ lib/test/auth_test.dart
@@ -121,7 +121,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
-    const user = AuthUser(isEmailVerified: false);
+    const user =
+        AuthUser(isEmailVerified: false, email: 'bhadangemihir07@gmail.com');
     _user = user;
     return Future.value(user);
   }
@@ -139,7 +140,8 @@ class MockAuthProvider implements AuthProvider {
     if (!isInitialized) throw NotInitializedException();
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
-    const newUser = AuthUser(isEmailVerified: true);
+    const newUser =
+        AuthUser(isEmailVerified: true, email: 'bhadangemihir07@gmail.com');
     _user = newUser;
   }
 }
