ab3fecff5deb91a191e1958be92f5426a3e9b166
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2023-11-28 14:14:17 +0530

    Update Existing Notes

--- lib/views/notes/notes_view.dart
+++ lib/views/notes/notes_view.dart
@@ -30,7 +30,7 @@ class _NotesViewState extends State<NotesView> {
         actions: [
           IconButton(
               onPressed: () {
-                Navigator.of(context).pushNamed(newNoteRoute);
+                Navigator.of(context).pushNamed(createOrUpdateNoteRoute);
               },
               icon: const Icon(Icons.add)),
           PopupMenuButton<MenuAction>(
@@ -74,8 +74,14 @@ class _NotesViewState extends State<NotesView> {
                         final allNotes = snapshot.data as List<DatabaseNote>;
                         return NotesListView(
                           notes: allNotes,
-                          onDeleteNote: (note)async{
+                          onDeleteNote: (note) async {
                             await _notesService.deleteNote(id: note.id);
+                          },
+                          onTap: (note) {
+                            Navigator.of(context).pushNamed(
+                              createOrUpdateNoteRoute,
+                              arguments: note,
+                            );
                           },
                         );
                       } else {
